fed071d23ecc72b1c296ccd8f25e19a76c23b2ed
sindris12 <sindris12@ru.is>
2014-09-23 22:01:07 +0000

    Finished about the app

--- code/src/ru/andr/SuperFreeFlow/AboutActivity.java
+++ code/src/ru/andr/SuperFreeFlow/AboutActivity.java
@@ -1,9 +1,7 @@
 package ru.andr.SuperFreeFlow;
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 /**
  * Created by sindrisigurjonsson on 23/09/14.
@@ -13,12 +11,6 @@ public class AboutActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
-
-
-        TextView txt = new TextView(AboutActivity.this);
-        txt.setText("Theodór Tómas Theodórsson\nÁrniFannar\nSindri Sigurjónsson");
-        txt.setLayoutParams(new ActionBar.LayoutParams(ActionBar.LayoutParams.FILL_PARENT,ActionBar.LayoutParams.WRAP_CONTENT));;
-        lin.addView(txt);
     }
 }
 
